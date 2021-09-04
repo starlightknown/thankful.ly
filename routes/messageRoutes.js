9f411330ac2c2f07c65c1ca9edabf06f1e409fcd
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-09-04 17:10:35 +0545

    Change to post request and remove controller

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const sendMessage = require('../controllers/messageController')
+// const sendMessage = require('../controllers/messageController')
 
 const router = express.Router()
 
 
-router.get('/sendMessage', (req, res) => {
-    let { sendTo, messageBody } = req.body
+router.post('/sendMessage', (req, res) => {
+    let { sendTo, messageBody } = {...req.body}
+    console.log(sendTo, messageBody)
     let accountSid = process.env.TWILIO_ACCOUNT_SID;
     let authToken = process.env.TWILIO_AUTH_TOKEN;
 
@@ -17,7 +18,8 @@ router.get('/sendMessage', (req, res) => {
             from: process.env.FROM,
             body: messageBody,
         })
-        .then(message => res.send(message.sid));
+        .then(message => res.send(message.sid))
+        .catch(err => res.status(500).json(err));
 })
 
 module.exports = router
