b65b92d27d50c07d453e32361fbbe9278a4c8038
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-09-04 17:08:06 +0545

    Make use of body parser and cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const messageRoutes = require('./routes/messageRoutes')
 require('dotenv').config()
+const cors = require("cors")
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
@@ -8,6 +10,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use(cors())
+app.use(bodyParser.json({type: 'application/json'}))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
